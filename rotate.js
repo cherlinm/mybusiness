c1322b8f25dd1528e7ae206ad7bafc1f9562a721
cherlinm <cherlinmao@gmail.com>
2022-07-27 10:56:12 -0700

    init

---
+++ rotate.js
@@ -0,0 +1,17 @@
+let imageIndex = 0;
+
+
+function startRotation() {
+  setInterval(() => {
+    rotate();
+  }, 2000);
+}
+
+function rotate() {
+  if (imageIndex === 6) {
+    imageIndex = 1
+  } else {
+    imageIndex ++;
+  }
+  document.getElementById('img-placeholder').src = 'images/' + imageIndex + '.jpg';
+}
